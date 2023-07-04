850fdd8ea7eedfca2a95a3ecede8cd71698cc067
Alina Miedviedieva <a.ndsowhattt@gmail.com>
2023-07-04 12:03:38 +0300

    cart js

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,7 @@
-
 import { toggleColor } from './modules/toggler.js';
 import { carouselSwitcher } from './modules/carousel.js';
 import { productsLink, randomProductsLink, randomSaleLink, fetchProducts } from './modules/shop.js';
 import { initCounter } from './modules/counter.js';
-
-
-
-
 
 document.getElementById('darkModeToggle').addEventListener('change', toggleColor);
 
--- src/js/modules/counter.js
+++ src/js/modules/counter.js
@@ -1,33 +1,36 @@
-// Отримуємо елементи з id "buyCount" і "wishlistCount" з хедеру
-const buyCountElement = document.getElementById('buyCount');
-const wishlistCountElement = document.getElementById('wishlistCount');
+export function initCounter() {
+	const buyCountElement = document.getElementById('buyCount');
+	const wishlistCountElement = document.getElementById('wishlistCount');
+	const cartIcon = document.querySelector('.buy');
 
-// Отримуємо збережені значення з localStorage або встановлюємо значення за замовчуванням
-let buyCount = localStorage.getItem('buyCount') || 0;
-let wishlistCount = localStorage.getItem('wishlistCount') || 0;
+	let buyCount = localStorage.getItem('buyCount') || 0;
+	let wishlistCount = localStorage.getItem('wishlistCount') || 0;
 
-// Функція для оновлення цифри в хедері та збереження значення в localStorage
-function updateCount(element, count, key) {
-  element.textContent = count > 0 ? count : '';
-  localStorage.setItem(key, count);
+	function updateCount(element, count, key) {
+		element.textContent = count > 0 ? count : '';
+		localStorage.setItem(key, count);
+
+		if (count > 0 && key === 'buyCount') {
+			cartIcon.classList.add('fa-beat');
+		} else {
+			cartIcon.classList.remove('fa-beat');
+		}
+	}
+
+	updateCount(buyCountElement, buyCount, 'buyCount');
+	updateCount(wishlistCountElement, wishlistCount, 'wishlistCount');
+
+	document.addEventListener('click', function (event) {
+		if (event.target.matches('.product-buy-actions .my-btn') && event.target.closest('.my-btn')) {
+			buyCount++;
+			updateCount(buyCountElement, buyCount, 'buyCount');
+		}
+	});
+
+	document.addEventListener('click', function (event) {
+		if (event.target.matches('.product-buy-actions .btn-secondary')) {
+			wishlistCount++;
+			updateCount(wishlistCountElement, wishlistCount, 'wishlistCount');
+		}
+	});	
 }
-
-// Оновлюємо значення цифр при завантаженні сторінки
-updateCount(buyCountElement, buyCount, 'buyCount');
-updateCount(wishlistCountElement, wishlistCount, 'wishlistCount');
-
-// Обробник події для кнопки "Buy"
-document.addEventListener('click', function(event) {
-  if (event.target.matches('.product-buy-actions .my-btn')) {
-    buyCount++; // Збільшуємо лічильник
-    updateCount(buyCountElement, buyCount, 'buyCount'); // Оновлюємо цифру в хедері та зберігаємо значення в localStorage
-  }
-});
-
-// Обробник події для кнопки "Wishlist this"
-document.addEventListener('click', function(event) {
-  if (event.target.matches('.product-buy-actions .btn-secondary')) {
-    wishlistCount++; // Збільшуємо лічильник
-    updateCount(wishlistCountElement, wishlistCount, 'wishlistCount'); // Оновлюємо цифру в хедері та зберігаємо значення в localStorage
-  }
-});
