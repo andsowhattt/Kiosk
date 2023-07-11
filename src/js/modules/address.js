5414ed2292cdee34436ea3ad6f822f781d534a40
Alina Miedviedieva <a.ndsowhattt@gmail.com>
2023-07-11 10:20:47 +0300

    adress js

--- src/js/modules/address.js
+++ src/js/modules/address.js
@@ -12,14 +12,14 @@ class DistrictSelector {
 			"Kropyvnytskyi": ["Kropyvnytskyi", "Golovanivskyi", "Novoukrainskyi", "Oleksandriyskyi"],
 			"Luhansk": ["Luhansk", "Alchevskyi", "Dovzhanskyi", "Rovenkivskyi", "Svativskyi", "Severodonetskyi", "Starobilsky", "Shchastinsky"],
 			"Lutsk": ["Lutsk", "Volodymyr-Volynskyi", "Kovel", "Novovolynsk"],
-			"Lviv": ["Lviv", "Drohobytskyi", "Zolochivskyi", "Sambirskyi", "Stryiskyi", "Chervonohradskyi", "Yavorivskyi"],		
-			"Mykolaiv": ["Mykolaiv", "Bashtan", "Voznesensky", "Pervomaysky"],	
-			"Odesa": ["Odesa","Berezivskyi", "Bilhorod-Dnistrovskyi", "Bolgradskyi", "Izmailskyi", "Podilskyi","Rozdilnianskyi"],
+			"Lviv": ["Lviv", "Drohobytskyi", "Zolochivskyi", "Sambirskyi", "Stryiskyi", "Chervonohradskyi", "Yavorivskyi"],
+			"Mykolaiv": ["Mykolaiv", "Bashtan", "Voznesensky", "Pervomaysky"],
+			"Odesa": ["Odesa", "Berezivskyi", "Bilhorod-Dnistrovskyi", "Bolgradskyi", "Izmailskyi", "Podilskyi", "Rozdilnianskyi"],
 			"Poltava": ["Poltava", "Kremenchutskyi", "Lubensky", "Myrhorodskyi"],
 			"Rivne": ["Rivne", "Varasky", "Dubensky", "Sarnensky"],
 			"Sumy": ["Sumy", "Konotopsky", "Shostkinsky", "Okhtyrsky", "Romensky"],
 			"Ternopil": ["Ternopil", "Kremenetsky", "Chortkivskyi"],
-			"Uzhhorod": ["Uzhhorod","Berehivskyi", "Mukachivskyi", "Rakhivskyi", "Tyachivskyi", "Khustskyi"],
+			"Uzhhorod": ["Uzhhorod", "Berehivskyi", "Mukachivskyi", "Rakhivskyi", "Tyachivskyi", "Khustskyi"],
 			"Kharkiv": ["Kharkiv", "Bogoduhivskyi", "Izyumskyi", "Krasnogradskyi", "Kupianskyi", "Lozovsky", "Chuguyivskyi"],
 			"Kherson": ["Kherson", "Beryslavskyi", "Henicheski", "Kakhovsky", "Skadovsky"],
 			"Khmelnytskyi": ["Khmelnytskyi", "Kamianets-Podilskyi", "Shepetivskyi"],
@@ -33,6 +33,9 @@ class DistrictSelector {
 
 		this.citySelect.addEventListener('change', this.updateDistricts.bind(this));
 		this.updateCityOptions();
+
+		this.citySelect.addEventListener('change', this.checkCustomOption.bind(this));
+		this.districtSelect.addEventListener('change', this.checkCustomOption.bind(this));
 	}
 
 	updateCityOptions() {
@@ -42,7 +45,13 @@ class DistrictSelector {
 			option.text = city;
 			this.citySelect.add(option);
 		}
+
+		const otherOption = document.createElement('option');
+		otherOption.value = 'Other';
+		otherOption.text = 'Other';
+		this.citySelect.add(otherOption);
 	}
+
 
 	updateDistricts() {
 		const selectedCity = this.citySelect.value;
@@ -64,6 +73,44 @@ class DistrictSelector {
 			option.text = district;
 			this.districtSelect.add(option);
 		});
+	}
+
+	checkCustomOption() {
+		if (
+			this.citySelect.value === 'Other' ||
+			this.districtSelect.value === 'Other'
+		) {
+			this.showCustomInputs();
+		} else {
+			this.hideCustomInputs();
+		}
+	}
+
+	showCustomInputs() {
+		const customCityInput = document.createElement('input');
+		customCityInput.type = 'text';
+		customCityInput.id = 'customCityInput';
+		customCityInput.classList.add('form-control');
+		customCityInput.placeholder = 'Enter your city';
+
+		const customDistrictInput = document.createElement('input');
+		customDistrictInput.type = 'text';
+		customDistrictInput.id = 'customDistrictInput';
+		customDistrictInput.classList.add('form-control');
+		customDistrictInput.placeholder = 'Enter your district';
+
+		this.citySelect.insertAdjacentElement('afterend', customCityInput);
+		this.districtSelect.insertAdjacentElement('afterend', customDistrictInput);
+	}
+
+	hideCustomInputs() {
+		const customCityInput = document.getElementById('customCityInput');
+		const customDistrictInput = document.getElementById('customDistrictInput');
+
+		if (customCityInput && customDistrictInput) {
+			customCityInput.remove();
+			customDistrictInput.remove();
+		}
 	}
 }
 
