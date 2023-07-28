f430392ab003fc8540455d9a54ca85614b5f7f44
Alina Miedviedieva <a.ndsowhattt@gmail.com>
2023-07-28 13:12:35 +0300

    refreshing info after checkout form

--- src/js/app.js
+++ src/js/app.js
@@ -6,38 +6,38 @@ import DistrictSelector from './modules/checkout.js';
 import { validateForm } from './modules/valid.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Додати відстеження подій кліків на кнопку "BUY"
-  const buyButton = document.querySelector('.btn-buy--js');
-  if (buyButton) {
-    buyButton.addEventListener('click', (event) => {
-      // Код, який виконується при кліку на кнопку "BUY"
-      // Ви можете залишити пустим, якщо не потрібно виконувати додаткові дії
-    });
-  }
+	// Додати відстеження подій кліків на кнопку "BUY"
+	const buyButton = document.querySelector('.btn-buy--js');
+	if (buyButton) {
+		buyButton.addEventListener('click', (event) => {
+			// Код, який виконується при кліку на кнопку "BUY"
+			// Ви можете залишити пустим, якщо не потрібно виконувати додаткові дії
+		});
+	}
 
-  // Перевірка на наявність контейнера ".cards" на головній сторінці
-  const cardsContainer = document.querySelector('.cards');
-  if (cardsContainer) {
-    fetchProducts(productsLink, '.cards');
-  }
+	// Перевірка на наявність контейнера ".cards" на головній сторінці
+	const cardsContainer = document.querySelector('.cards');
+	if (cardsContainer) {
+		fetchProducts(productsLink, '.cards');
+	}
 
-  // Перевірка на наявність контейнера ".random__cards" на сторінці каталогу
-  const randomCardsContainer = document.querySelector('.random__cards');
-  if (randomCardsContainer) {
-    fetchProducts(randomProductsLink, '.random__cards');
-  }
+	// Перевірка на наявність контейнера ".random__cards" на сторінці каталогу
+	const randomCardsContainer = document.querySelector('.random__cards');
+	if (randomCardsContainer) {
+		fetchProducts(randomProductsLink, '.random__cards');
+	}
 
-  // Перевірка на наявність контейнера ".sale__cards" на сторінці каталогу
-  const saleCardsContainer = document.querySelector('.sale__cards');
-  if (saleCardsContainer) {
-    fetchProducts(randomSaleLink, '.sale__cards');
-  }
+	// Перевірка на наявність контейнера ".sale__cards" на сторінці каталогу
+	const saleCardsContainer = document.querySelector('.sale__cards');
+	if (saleCardsContainer) {
+		fetchProducts(randomSaleLink, '.sale__cards');
+	}
 
-  validateForm();
+	validateForm();
 
-  const districtSelector = new DistrictSelector('city', 'district', 'delivery');
+	const districtSelector = new DistrictSelector('city', 'district', 'delivery');
 
-  new Counter();
+	new Counter();
 
-  carouselSwitcher();
+	carouselSwitcher();
 });
--- src/js/modules/valid.js
+++ src/js/modules/valid.js
@@ -6,6 +6,9 @@ export function validateForm() {
 		form.addEventListener('submit', function (event) {
 			event.preventDefault();
 			if (form.checkValidity()) {
+				// Виклик функції для оновлення іконки корзини після підтвердження замовлення
+				updateCartAfterCheckout();
+
 				window.location.href = "thx.html";
 			} else {
 				event.stopPropagation();
@@ -13,4 +16,18 @@ export function validateForm() {
 			}
 		});
 	}
+}
+
+function updateCartAfterCheckout() {
+	const cartCountElement = document.getElementById('buyCount');
+	const currentCartCount = parseInt(cartCountElement.textContent) || 0;
+
+	// Оновити цифру на іконці корзини
+	cartCountElement.textContent = '';
+
+	// Зберегти оновлену кількість товарів у локальне сховище
+	localStorage.setItem('buyCount', '0');
+
+	// Очистити вміст корзини у локальному сховищі
+	localStorage.removeItem('lastItems');
 }
