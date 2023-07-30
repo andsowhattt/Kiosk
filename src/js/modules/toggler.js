27659aaaf19fce9c395f550f2bd32d99d26ef8c4
Alina Miedviedieva <a.ndsowhattt@gmail.com>
2023-07-30 16:37:08 +0300

    toggler destructurisation

--- src/js/modules/toggler.js
+++ src/js/modules/toggler.js
@@ -2,11 +2,9 @@ const darkModeToggle = document.getElementById('darkModeToggle');
 const menuItems = document.querySelectorAll('.nav-link');
 
 const isDarkMode = localStorage.getItem('darkMode') === 'true';
-
-darkModeToggle.checked = isDarkMode;
 setDarkMode(isDarkMode);
 
-darkModeToggle.addEventListener('change', function () {
+darkModeToggle.addEventListener('change', () => {
 	const isDarkMode = darkModeToggle.checked;
 	setDarkMode(isDarkMode);
 	localStorage.setItem('darkMode', isDarkMode);
@@ -14,7 +12,7 @@ darkModeToggle.addEventListener('change', function () {
 
 function setDarkMode(isDarkMode) {
 	document.body.classList.toggle('dark-mode', isDarkMode);
-	menuItems.forEach(function (menuItem) {
+	menuItems.forEach(menuItem => {
 		menuItem.style.color = isDarkMode ? 'white' : 'black';
 	});
 }
