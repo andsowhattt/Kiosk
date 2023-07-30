8cfe5962e51c8407c51022a476cff1a8cb0d4343
Alina Miedviedieva <a.ndsowhattt@gmail.com>
2023-07-30 16:44:39 +0300

    validation destructurisation

--- src/js/modules/valid.js
+++ src/js/modules/valid.js
@@ -1,33 +1,26 @@
 export function validateForm() {
 	const form = document.querySelector('.needs-validation');
-	const submitBtn = document.querySelector('button[type="submit"]');
+	const submitBtn = document.querySelector('.confirm-button--js');
 
-	if (form) {
-		form.addEventListener('submit', function (event) {
-			event.preventDefault();
-			if (form.checkValidity()) {
-				// Виклик функції для оновлення іконки корзини після підтвердження замовлення
-				updateCartAfterCheckout();
+	if (!form) return;
 
-				window.location.href = "thx.html";
-			} else {
-				event.stopPropagation();
-				form.classList.add('was-validated');
-			}
-		});
-	}
+	form.addEventListener('submit', function (event) {
+		event.preventDefault();
+		if (form.checkValidity()) {
+			updateCartAfterCheckout();
+			window.location.href = "thx.html";
+		} else {
+			event.stopPropagation();
+			form.classList.add('was-validated');
+		}
+	});
 }
 
 function updateCartAfterCheckout() {
 	const cartCountElement = document.getElementById('buyCount');
 	const currentCartCount = parseInt(cartCountElement.textContent) || 0;
 
-	// Оновити цифру на іконці корзини
 	cartCountElement.textContent = '';
-
-	// Зберегти оновлену кількість товарів у локальне сховище
 	localStorage.setItem('buyCount', '0');
-
-	// Очистити вміст корзини у локальному сховищі
 	localStorage.removeItem('lastItems');
 }
