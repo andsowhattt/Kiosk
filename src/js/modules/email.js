37a23167fc5e73e878b5bd16c39d95573d8d4053
Alina Miedviedieva <a.ndsowhattt@gmail.com>
2023-07-30 16:32:30 +0300

    email destructurisation

--- src/js/modules/email.js
+++ src/js/modules/email.js
@@ -1,27 +1,29 @@
-export function sendMessage() {
+export async function sendMessage() {
 	const form = document.querySelector('.message__form--js');
 	const formData = new FormData(form);
 
-	fetch('https://jsonplaceholder.typicode.com/posts', {
-		method: 'POST',
-		body: formData
-	})
-		.then(response => {
-			if (!response.ok) {
-				throw new Error('Error: ' + response.status);
-			}
-			return response.json();
-		})
-		.then(data => {
-			console.log('Message sent successfully:', data);
-			form.reset();
-
-			const formDataObject = Object.fromEntries(formData.entries());
-
-			alert(JSON.stringify(formDataObject));
-			alert('Message sent successfully');
-		})
-		.catch(error => {
-			console.error('Error:', error);
+	try {
+		const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+			method: 'POST',
+			body: formData,
 		});
-}	 
+
+		if (!response.ok) {
+			throw new Error('Error: ' + response.status);
+		}
+
+		const data = await response.json();
+
+		console.log('Message sent successfully:', data);
+		form.reset();
+
+		const formDataObject = Object.fromEntries(formData.entries());
+
+		alert(JSON.stringify(formDataObject));
+		alert('Message sent successfully');
+
+		return data; 
+	} catch (error) {
+		console.error('Error:', error);
+	}
+}
