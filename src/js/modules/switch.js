10a61da9134ffdb02176f0f80d7f112704f5ca79
Alina Miedviedieva <a.ndsowhattt@gmail.com>
2023-08-02 18:02:25 +0300

    dark mode edited

--- src/js/modules/switch.js
+++ src/js/modules/switch.js
@@ -1,6 +1,5 @@
 const darkModeToggle = document.querySelector('.switch__dark');
 const menuItems = document.querySelectorAll('.bar__list-link');
-const modalContent = document.querySelector('.modal-content--js');
 
 const isDarkMode = localStorage.getItem('darkMode') === 'true';
 darkModeToggle.checked = isDarkMode;
@@ -16,6 +15,5 @@ function setDarkMode(isDarkMode) {
 	document.body.classList.toggle('dark-mode', isDarkMode);
 	menuItems.forEach(menuItem => {
 		menuItem.style.color = isDarkMode ? 'white' : 'black';
-	});
-	modalContent.style.color = isDarkMode ? 'white' : 'black';
+	}); 
 }
