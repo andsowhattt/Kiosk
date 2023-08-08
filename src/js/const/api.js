5144fa8cdbb168a6882a202b7200036749135063
Alina Miedviedieva <a.ndsowhattt@gmail.com>
2023-08-08 12:51:17 +0300

    api links edited

--- src/js/const/api.js
+++ src/js/const/api.js
@@ -1,5 +1,8 @@
 export const apiLinks = {
 	products: 'https://api.escuelajs.co/api/v1/products',
-	randomProducts: 'https://api.escuelajs.co/api/v1/products?offset=0&limit=3',
-	randomSale: 'https://api.escuelajs.co/api/v1/products?offset=0&limit=6',
+	randomProducts: '?offset=0&limit=3',
+	randomSale: '?offset=0&limit=6',
 };
+
+apiLinks.randomProducts = apiLinks.products + apiLinks.randomProducts;
+apiLinks.randomSale = apiLinks.products + apiLinks.randomSale;
