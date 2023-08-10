b7a2ca5b1f2af2f80d49720888d71d0c5033dc0c
Alina Miedviedieva <a.ndsowhattt@gmail.com>
2023-08-10 16:29:12 +0300

    local instal booot, add swiper

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,12 @@
+import 'bootstrap';
 import './modules/mode.js';
-import { carouselSwitcher } from './modules/carousel.js';
+import './modules/slider.js';
 import { Counter } from './modules/counter.js';
 import CheckOutForm from './modules/checkout.js';
 import { validateForm } from './modules/valid.js';
 import { initEmailForm } from './modules/email.js';
 import { initializeWishlist } from './modules/wishlist.js';
 import { fetchAllProducts } from './modules/productFetcher.js'; 
-
 document.addEventListener('DOMContentLoaded', () => {
 
 	fetchAllProducts(); 
@@ -18,10 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	const counter = new Counter();
 	counter.init();
 
-	carouselSwitcher();
-
 	initEmailForm();
 
 	initializeWishlist(); 
 });
-
--- src/js/modules/carousel.js
+++
@@ -1,9 +0,0 @@
-export function carouselSwitcher() {
-	$(".carousel__prev").click(function () {
-		$(".carousel").carousel("prev");
-	});
-
-	$(".carousel__next").click(function () {
-		$(".carousel").carousel("next");
-	});
-}
