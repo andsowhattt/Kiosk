f25f68630fa4868161578841c5c04cad212f1f6f
Alina Miedviedieva <a.ndsowhattt@gmail.com>
2023-08-12 13:53:41 +0300

    favicon, name of pages, api links edited

--- src/js/const/api.js
+++ src/js/const/api.js
@@ -1,8 +1,9 @@
 export const apiLinks = {
-	products: 'https://api.escuelajs.co/api/v1/products',
+	api: 'https://api.escuelajs.co/api/v1/products',
+	products: '?offset=0&limit=48',
 	randomProducts: '?offset=0&limit=6',
 	randomSale: '?offset=0&limit=6',
 };
-
-apiLinks.randomProducts = apiLinks.products + apiLinks.randomProducts;
-apiLinks.randomSale = apiLinks.products + apiLinks.randomSale;
+apiLinks.products = apiLinks.api + apiLinks.products;
+apiLinks.randomProducts = apiLinks.api + apiLinks.randomProducts;
+apiLinks.randomSale = apiLinks.api + apiLinks.randomSale;
