5603b00dced34720bbf1bb3e26b17b827c69cc3a
Alina Miedviedieva <a.ndsowhattt@gmail.com>
2023-08-12 16:34:26 +0300

    carousel edited, img card height edited

--- src/js/modules/carousel.js
+++ src/js/modules/carousel.js
@@ -1,14 +1,10 @@
-import Swiper, { Navigation, Autoplay } from 'swiper';
+import Swiper, { Autoplay } from 'swiper';
 
-Swiper.use([Navigation, Autoplay]);
+Swiper.use([ Autoplay]);
 
 const swiper = new Swiper('.carousel--js', {
 	loop: true,
 	speed: 1000,
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
 	autoplay: {
 		delay: 3000, 
 		disableOnInteraction: false, 
